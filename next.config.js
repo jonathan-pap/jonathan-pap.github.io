1c1b1af5a6b5f450e6d850c096df800b50bff68e
Jonathan Papworth <trinneh@gmail.com>
2024-09-05 14:36:22 +0200

    s

--- next.config.js
+++ next.config.js
@@ -1,31 +1,9 @@
 /**
- //* @type {import('next').NextConfig}
- */
-
-//const nextConfig = {
-//  reactStrictMode: true,
-//};
-
-//module.exports = nextConfig;
-
-
-/**
  * @type {import('next').NextConfig}
  */
+
 const nextConfig = {
   reactStrictMode: true,
-  
-  // Enable static export
-  output: 'export',
-  
-  // GitHub Pages does not support image optimization
-  images: {
-    unoptimized: true,
-  },
-  
-  // Specify the base path if your project is not at the root of the domain
-  basePath: '/jonathan-pap.github.io',  // Replace with your repository name
-  assetPrefix: '/your-repo-name/',  // Ensure assets are correctly linked
 };
 
 module.exports = nextConfig;
